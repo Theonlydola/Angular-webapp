466874b95ff41fe41c835ef0edc157938d6b0623
theonlydola <muhammeddadel@gmail.com>
2021-11-15 12:24:55 +0200

    first commit

---
+++ target/liberty/wlp/lib/features/checksums/com.ibm.websphere.appserver.servlet-servletSpi1.0.cs
@@ -0,0 +1,4 @@
+#Sun Nov 14 16:00:49 EET 2021
+dev/spi/ibm/javadoc/com.ibm.websphere.appserver.spi.servlet_2.9-javadoc.zip=9ef618802d257d27929c528d51168adb
+lib/features/com.ibm.websphere.appserver.servlet-servletSpi1.0.mf=e8944c026fd8a6c1d55176d0b3fe4774
+dev/spi/ibm/com.ibm.websphere.appserver.spi.servlet_2.9.58.jar=0708a5a60d8532ce7c60376c460f5ac4
---
+++ target/liberty/wlp/lib/features/checksums/io.openliberty.servlet.api-3.1.cs
@@ -0,0 +1,3 @@
+#Sun Nov 14 16:00:48 EET 2021
+lib/features/io.openliberty.servlet.api-3.1.mf=27fc8eebc020c32206534bd75bb9c870
+dev/api/spec/com.ibm.websphere.javaee.servlet.3.1_1.0.58.jar=f5a713bfff71658a6b0f78932f182f1f
